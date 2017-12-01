996289dcd63e4e397328eeb9a6151dcb702b58de
abetomo <abe@enzou.tokyo>
2017-12-01 16:24:23 +0900

    Add test

---
+++ src/__tests__/index.js
@@ -0,0 +1,15 @@
+'use strict'
+
+process.stdout.columns = 5
+const getStringFullWidthOfTerminal = require('../').getStringFullWidthOfTerminal
+
+/* global describe, test, expect */
+describe('getStringFullWidthOfTerminal()', () => {
+  test('When the argument is one character', () => {
+    expect(getStringFullWidthOfTerminal('=')).toBe('====')
+  })
+
+  test('When the argument is more than one character', () => {
+    expect(getStringFullWidthOfTerminal('hoge')).toBe('hogeh')
+  })
+})
